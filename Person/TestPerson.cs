cae21c3ba8ea418462063162e92b0519520a3d6c
mathkarl <mathkarl@LSE-B2725>
2020-01-29 12:49:07 +0000

    Add project files.

---
+++ Person/TestPerson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    class TestPerson
+    {
+        public string firstname;
+        public string secondname;
+
+        public string GetFullName()
+        {
+            return firstname + secondname;
+        }
+    }
+}
